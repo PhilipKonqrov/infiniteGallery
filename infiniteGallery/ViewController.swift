0efef4946b14875ad8823d01b01a260df4ac5b28
PhilipKonqrov <filip@lionheartbulgaria.io>
2022-03-26 03:20:28 +0200

    Initial Commit

---
+++ infiniteGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  infiniteGallery
+//
+//  Created by Philip Plamenov on 26.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
